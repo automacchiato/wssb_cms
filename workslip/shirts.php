375428f846103f089596d1a3c006472d7e4a744e
Ramadani Anwar Sabaruddin <danisabaruddin98@gmail.com>
2026-04-25 17:00:02 +0800

    add armhole

--- workslip/shirts.php
+++ workslip/shirts.php
@@ -375,6 +375,7 @@ if (isset($_POST['submit'])) {
                                 <h6 class="mt-auto mb-auto">Shoulder</h6>
                                 <h6 class="mt-auto mb-auto">Sleeve Length</h6>
                                 <h6 class="mt-auto mb-auto">Arm Length</h6>
+                                <h6 class="mt-auto mb-auto">Arm Hole</h6>
                             </div>
                             <div class="col-md-3">
                                 <h5 class="text-center">Fit</h5>
